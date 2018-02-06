60319d61eec90e00cb6e5edd6cd5438afed4057c
Deep Patel <potlee@me.com>
2018-02-06 17:37:25 +0100

    0.5% loss

--- viz.py
+++ viz.py
@@ -1,10 +1,11 @@
 import seaborn as sns
 %matplotlib inline
 
-matrix = [[] for x in languages]
+matrix = []
 for i, lang1 in enumerate(languages):
-    for lang2 in languages:
-        matrix[i].append(score(corpus[i], lang2))
-matrix = pd.DataFrame(matrix, columns=languages)
-sns.set(rc={'figure.figsize':(14,9)})
-sns.heatmap(matrix, center=100, yticklabels=languages)
+    matrix.append(scoresV2(corpus[i])[1].tolist()[0])
+matrix = pd.DataFrame(np.array(matrix), columns=languages)
+sns.set(rc={'figure.figsize':(20,12)})
+sns.set(font_scale=2)
+matrix=(matrix-matrix.mean())/matrix.std()
+sns.heatmap(matrix, center=1.5, yticklabels=languages, cmap="viridis_r")
